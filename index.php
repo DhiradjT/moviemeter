5515071c3c17294611db4468e907833dc0d12796
Dhiradj Tangali <90622013+DhiradjT@users.noreply.github.com>
2022-11-23 12:56:13 +0100

    footer a href links bijgewerkt
    - verder gewerkt aan de links in de footer
    - verder gewerkt aan de footer e.g

--- index.php
+++ index.php
@@ -245,31 +245,14 @@
 
   <div class="col-md-12 text-center text-light"> <div class="sep">HERE TO GIVE YOU THAT LITTLE PUSH</div></div>
   <div class="col-md-12 text-center text-light"><h2 class="brand2">Start sharing your Experience today.</h2></div>
-  <div class="col-md-12 mb-5 text-center"><button type="button" class="btn btn-outline-warning">Sign In</button></div>
+  <div class="col-md-12 mb-5 text-center"><button type="button" class="btn btn-outline-warning rounded">Sign In</button></div>
 
 
-
-
-<div class="col-md-3"><h2 class="brand text-light">Movie <span class="sep">Meter</span></h2><p class="business-text">Copyright	&#169; 2022 MovieMeter, <br> All Rights Reserved</p></div>
-<div class="col-md-3"><a href="#">Home</a> <a href="#">Categories</a> <br> <a href="#">Reviews</a> <a href="#">About</a> <br> <a href="#">Contact</a></div>
+<div class="col-md-3"><h2 class="brand text-light">Movie <span class="sep">Meter</span></h2><p class="business-text">Copyright	&#169;2022 MovieMeter, <br> All Rights Reserved</p></div>
+<div class="col-md-3"><a href="#">Home</a>  <a href="#">Categories</a> <br> <a href="#">Reviews</a> <a href="#">About</a> <br> <a href="#">Contact</a> <a href="#">Sign In</a></div>
 <div class="col-md-3"><a href="#">Terms and Conditions</a> <br> <a href="#">Privacy Policy</a> <br> <a href="#">Cookie Policy</a></div>
-<div class="col-md-3"><p class="text-light">Need Help?</p><p class="business-text">help@movie.co</p><br><a href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a> <a href="https://twitter.com/"><i class="bi bi-twitter"></i></a><a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a></div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div class="col-md-3"><span class="text-light help-text">Need Help?</span><br><span class="business-text">
+<a href="mailto:help@movie.co">help@movie.co</a></span><br><a href="https://www.instagram.com/" target="_blank"><i class="bi bi-instagram"></i></a> <a href="https://twitter.com/" target="_blank"><i class="bi bi-twitter"></i></a><a href="https://www.facebook.com/" target="_blank"><i class="bi bi-facebook"></i></a></div>
 
 
     </div>
