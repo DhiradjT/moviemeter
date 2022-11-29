ecf750003f1bb2841c9b29028d6b1e1a7d06f057
Dhiradj Tangali <90622013+DhiradjT@users.noreply.github.com>
2022-11-29 12:05:17 +0100

    begin gemaakt aan de reviews pagina

---
+++ blackpanther.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieMeter - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar fixed-top navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">Movie <span class="sep">Meter</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+      <ul class="navbar-nav ms-auto">
+        
+        <li class="nav-item">
+          <a class="nav-link text-light" aria-current="page" href="#">Home</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categories
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item " href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Comedy</a></li>
+            <li><a class="dropdown-item" href="#">Horror</a></li>
+            <li><a class="dropdown-item" href="#">Kids</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Reviews</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Contact</a>
+        </li>
+
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+
+<!-- <div
+
+class="p-3 text-center bg-image"
+
+style="
+
+  background-image: url('./movie-headers/blackpanther.jpg');
+  height: 400px;
+
+"   
+
+>
+
+<div class="mask">
+
+  <div class="d-flex justify-content-center align-items-center w-60">
+
+    <div class="text-white">
+
+    </div>
+
+  </div>
+
+</div>
+
+</div> -->
+
+
+
+<div class="movie-info">
+    <div class="container">
+        <div class="row">
+            <h2 class="col-md-12 my-3 movie-title text-light">BLACK PANTHER: WAKANDA FOREVER</h2>
+
+        <div class="col-md-8 text-muted">In Marvel Studios’ Black Panther: Wakanda Forever, strijden koningin Ramonda (Angela Bassett), Shuri (Letitia Wright), M'Baku (Winston Duke), Okoye (Danai Gurira) en de Dora Milaje (waaronder Florence Kasumba) na de dood van Koning T’Challa om hun koninkrijk te beschermen.</div>
+
+        <h2 class="col-md-12 my-5 text-light">Reviews</h2>
+
+
+
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
