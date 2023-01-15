ecc94f9915d889eb707eec43cde1a4b50389d01d
Dhiradj <dhiradj2004@gmail.com>
2023-01-15 23:37:38 +0100

    login pagina en about pagina

---
+++ about.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieMeter - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">Movie <span class="sep">Meter</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+      <ul class="navbar-nav ms-auto">
+        
+        <li class="nav-item">
+          <a class="nav-link text-light" aria-current="page" href="index.php">Home</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link text-light dropdown-toggle" href="categories.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categories
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="action.php">Action</a></li>
+            <li><a class="dropdown-item" href="comedy.php">Comedy</a></li>
+            <li><a class="dropdown-item" href="horror.php">Horror</a></li>
+            <li><a class="dropdown-item" href="kids.php">Kids</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-light" href="reviews.php">Reviews</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-light" href="about.php">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-light" href="signin.php">Sign In</a>
+        </li>
+
+      
+
+
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="container">
+    <div class="row">
+
+    <div class="col-md-6 mt-5">
+        <h2 class="about-h2">A movie application <br> that wins.</h2>
+        <p class="about-p">Welcome to our movie review website! We are a group of three <br> movie enthusiasts who have a passion for designing and <br> developing websites. On our site, you'll find a collection of <br> reviews written by people like you, who have watched a wide variety <br> of films and want to share their thoughts and <br> experiences with others.</p>
+        <div class="read-more1"><button type="button" class="btn btn-warning rounded">Read More</button></div>
+
+    </div>
+
+
+    <div class="col-md-6">
+        <div class="hero-about"><img src="./stock-photos/family.png" alt="family" class="img-fluid"></div>
+    </div>
+
+
+     </div> <!-- end row -->
+</div> <!-- end container -->
+
+
+
+
+
+
+<div class="more-section">
+  <div class="container">
+    <div class="row">
+
+    <div class="col-md-12 mb-5 text-center text-light">Why Choose MovieMeter?</div>
+
+    <div class="col-md-4"><div class="icon text-center"><img src="./icons/24-hours.png" alt="chat"></div>
+<p class="sup-text text-center">Amazing 24/7 <br>
+Support Team</p>
+</div>
+
+<div class="col-md-4"><div class="icon"><img src="./icons/rating.png" alt="happy"></div>
+<p class="sup-text text-center">Family Friendly <br> 
+Comunity</p>
+</div>
+
+<div class="col-md-4"><div class="icon"><img src="./icons/consumer.png" alt="chat"></div>
+<p class="sup-text text-center">Happy <br>
+ Customers 
+</p>
+</div>
+
+
+
+
+
+
+
+
+        
+  </div> <!-- end row -->
+ </div> <!-- end container -->
+</div> <!-- end  more section div -->
+
+
+<div class="end">
+<div class="container ">
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="about-h2">We're glad you found it <br> engaging and <br> informative.</h2>
+        </div>
+
+        <div class="col-md-6">
+        <p class="about-p">Our reviews cover a wide range of genres, from action and adventure to comedy and drama, and we strive to provide honest and informative assessments of the movies we watch. We believe that everyone's opinion is valid, and we encourage our readers to share their own thoughts and ratings in the comments section.</p>
+        <div class="read-more1"><button type="button" class="btn btn-warning rounded">Get Started</button></div>    
+    </div>
+        
+
+
+    </div>
+</div>
+</div>
